174d536ad40d51424751fa3998b19010a188d366
sivapc <contactsiva.spc@gmail@gmail.com>
2018-04-26 12:18:05 +0530

    setup instructions

---
+++ videoscript/install.sh
@@ -0,0 +1,19 @@
+# Installation
+sudo add-apt-repository -y ppa:mc3man/trusty-media
+sudo add-apt-repository -y ppa:kirillshkrogalev/ffmpeg-next
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt -y dist-upgrade
+sudo apt install -y python2.7 python-pip
+sudo apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+sudo apt-get install -y ffmpeg
+sudo apt-get install -y frei0r-plugins
+sudo pip install -y os0
+sudo pip install -y natsort
+sudo pip install -y jsonlib
+sudo pip install -y google-streetview
+sudo apt-get -y update
+sudo pip install -y moviepy
+sudo pip install -y Flask
+sudo pip install -y Flask-Cors 
+sudo pip install -y pprint 
