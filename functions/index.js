217ccc9b24e2c0d4e6a93724708403736016bfb2
Sandip Das <sandip.sandip.das5@gmail.com>
2018-06-26 23:16:31 +0530

    aminities changes and new api for latest resource import

--- functions/index.js
+++ functions/index.js
@@ -10,6 +10,7 @@ function getConfig(){
   const ref = firebaseApp.database().ref('integrations');
   return ref.once("value").then(snap=>snap.val());
 }
+
 getConfig().then((mainConfigs) => {
   console.log('check below configs');
   console.log(mainConfigs);
@@ -75,11 +76,89 @@ app.get('/api/coords/:lat/:lon', (req, res) => {
   });
 });
 
-app.post('/api/route-details/:route_id', (req, res) => {
-    return res.status(ok).json({
-      status:'ok',
-      message:'success'
+app.post('/api/route-details/:route_code/:direction', (req, res) => {
+  try{
+    const route_code = req.params.route_code;
+    const direction =  req.params.direction;
+    if(!route_code){
+      return res.status(400).json({
+        status:'error',
+        message:'please specify route code in url'
+      });
+    }
+    if(!direction || (direction !=='a' && direction !=='b' )){
+      return res.status(400).json({
+        status:'error',
+        message:'please specify direction code in url e.g. a or b'
+      });
+    }
+
+      const latestResources = req.body.latestResources;
+      if(!latestResources){
+        return res.status(400).json({
+          status:'error',
+          message:'latestResources node not present in request body'
+        });
+      }
+
+      if(!latestResources.videoUrl){
+        return res.status(400).json({
+          status:'error',
+          message:'videoUrl node not present inside latestResources node'
+        });
+      }
+      if(!latestResources.videoLeftUrl){
+        return res.status(400).json({
+          status:'error',
+          message:'videoLeftUrl node not present inside latestResources node'
+        });
+      }
+      if(!latestResources.videoRightUrl){
+        return res.status(400).json({
+          status:'error',
+          message:'videoRightUrl node not present inside latestResources node'
+        });
+      }
+      if(!latestResources.videoBackUrl){
+        return res.status(400).json({
+          status:'error',
+          message:'videoBackUrl node not present inside latestResources node'
+        });
+      }
+      if(!latestResources.KmlUrl){
+        return res.status(400).json({
+          status:'error',
+          message:'KmlUrl node not present inside latestResources node'
+        });
+      }
+
+
+      const ref = firebaseApp.database().ref();
+      const update = {};
+      update['route-details/'+route_code+'/latestResources/'+direction] = latestResources;
+      ref.update(update, (error) => {
+        if (error) {
+          console.log("Error updating data:", error);
+          return res.status(500).json({
+            status:'ok',
+            message:'error while saving the data in firebase'
+          });
+        }else{
+          console.log('Write Success');
+        }
+        return res.status(200).json({
+          status:'ok',
+          message:'resources updated successfully'
+        });
+      });
+  }catch(err){
+    return res.status(400).json({
+      status:'error',
+      message:err.message,
+      error_data: err.stack
     });
+  }
+
 });
 // app.get('/api/2',(request,response) => {
 //  response.send("Hello 2 from Firebase!");
